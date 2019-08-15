487681eb0d74d935296e6c5e91d175c28b379dac
BENACHIR Khalil <benachir1996khalil@gmail.com>
2019-08-15 13:53:03 +0100

    add mapview.callout ,item and input

--- App.js
+++ App.js
@@ -1,41 +1,21 @@
 import React from "react";
 import Home from "./home";
 import SearchBox from "./component/searchBox/searchBox";
-import { Item, Input } from "native-base";
-import Icon from "react-native-vector-icons/FontAwesome";
 
 import store from "./redux/store";
 import { Provider } from "react-redux";
 /* @flow */
 
-import { View, StyleSheet } from "react-native";
-
 class App extends React.Component {
+  
   render() {
-    Icon.loadFont();
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Item>
-            <Input placeholder="Icon Textbox" />
-            <Icon name="rocket" size={30} color="#900" />
-          </Item>
-        </View>
+        <Home />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
-});
-
-
-
-
-
 
 
 export default App;
--- home.js
+++ home.js
@@ -1,6 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text, Button, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Dimensions,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 
+import { Item, Input } from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
 import MapView from "react-native-maps";
 
 import {
@@ -18,6 +28,7 @@ class Home extends React.Component {
   componentDidMount() {
     const { dispatch1, location, getPredictions } = this.props;
     dispatch1();
+    Icon.loadFont();
 
     console.log("-------test");
     //  getPredictions();
@@ -28,26 +39,107 @@ class Home extends React.Component {
     const { location } = this.props;
     console.log("-------location-------", typeof location.latitude);
     return (
-      <MapView
-        style={{ flex: 1 }}
-        region={{
-          latitude: location.latitude,
-          longitude: location.longitude,
-          latitudeDelta: LATITUDE_DELTA,
-          longitudeDelta: LONGITUDE_DELTA
-        }}
-      >
-        <MapView.Marker
-          coordinate={{
-            latitude: location.latitude ,
-            longitude: location.longitude
+      <View style={{flex: 1}}>
+        <MapView
+          style={{ flex: 1 }}
+          region={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
           }}
-
-        />
-      </MapView>
+          showsUserLocation={true}
+        >
+          <MapView.Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude
+            }}
+          />
+        </MapView>
+        <MapView.Callout style={styles.searchCallout}>
+          <Item style={styles.calloutSearch}>
+            <Icon active name='home' />
+            <Input placeholder='Icon Textbox'/>
+          </Item>
+          <Item style={styles.calloutSearchLatChild}>
+            <Icon active name='home' />
+            <Input placeholder='Icon Textbox'/>
+          </Item>
+        </MapView.Callout>
+        <MapView.Callout style={styles.buttonCallout}>
+          <TouchableOpacity
+            style={[styles.touchable]}
+            onPress={() => console.log("press")}
+          >
+            <Text style={styles.touchableText}>Press Me 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.touchable]}
+            onPress={() => console.log("press")}
+          >
+            <Text style={styles.touchableText}>Press Me 2</Text>
+          </TouchableOpacity>
+        </MapView.Callout>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  buttonCallout: {
+    flex: 1,
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 10,
+    alignSelf: "center",
+    justifyContent: "space-between",
+    backgroundColor: "transparent",
+    borderWidth: 0.5,
+    borderRadius: 20
+  },
+  touchable: {
+    backgroundColor: "lightblue",
+    padding: 10,
+    margin: 10
+  },
+  touchableText: {
+    fontSize: 24
+  },
+  searchCallout: {
+    flexDirection: "column",
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 10,
+    width: "80%",
+    marginLeft: "10%",
+    marginTop: 40
+  },
+  calloutSearch: {
+    marginLeft: 10,
+    width: "90%",
+    marginRight: 10,
+    height: 40,
+    borderWidth: 0.0,
+    textAlign: "center",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderColor: "rgba(120, 230, 40, 0.8)"
+  },
+  calloutSearchLatChild: {
+    borderColor: "transparent",
+    marginLeft: 10,
+    width: "90%",
+    marginRight: 10,
+    height: 40,
+    borderWidth: 0.0,
+    textAlign: "center",
+    marginBottom: 10,
+  }
+});
+
 //parseFloat(location.coords.latitude)
 //location.coords.latitude
 const mapStateToProps = state => ({
